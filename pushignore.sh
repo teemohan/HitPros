e98681c6df67994c28df0fb16321469bef9eb67d
teemohan <teemo.kele@gmail.com>
2025-03-17 10:59:50 +0800

    更新代码

--- pushignore.sh
+++ pushignore.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+CURRENT_DIR=$(pwd)
+# 组合成完整的本地绝对路径
+BASE_PATH="${CURRENT_DIR}/"
 
 # 创建或更新 .shopifyignore 文件
-echo "templates/*" > shopify/.shopifyignore
-echo "config/*" >> shopify/.shopifyignore
-echo "locales/*" >> shopify/.shopifyignore
-echo ".shopify/*" >> shopify/.shopifyignore
+shopify_dir="${BASE_PATH}shopify/"
+echo "Navigating to shopify directory: $shopify_dir"
 
-# 如果 shopify 目录不存在，则切换到该目录
-if [ ! -d "shopify" ]; then
-    cd shopify || exit 1
-fi
+# 创建或更新 .shopifyignore 文件
+echo "templates/*" > "${shopify_dir}.shopifyignore"
+echo "config/*" >> "${shopify_dir}.shopifyignore"
+echo "locales/*" >> "${shopify_dir}.shopifyignore"
+echo ".shopify/*" >> "${shopify_dir}.shopifyignore"
 
 # 推送主题
-shopify theme push
+echo "Pushing Shopify theme..."
+shopify theme push --path="$shopify_dir"
+if [ $? -ne 0 ]; then
+  echo "Error pushing Shopify theme"
+  exit 1
+fi
